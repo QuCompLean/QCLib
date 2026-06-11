dc59cd3cfb6e99a6fb8184ec29c453c572d24250
DHTT <prparadoxes@gmail.com>
2026-06-12 01:16:52 +0200

    .

--- QCLib/Circuit/QFT.lean
+++ QCLib/Circuit/QFT.lean
@@ -54,11 +54,11 @@ section QFT
 
 open Register
 
+
 def QFTInv (n : ℕ) : 𝐔[Register n] :=
   ⟨√(2^n)⁻¹ • of fun a b => (starRingEnd ℂ) (ζ (2^n) ^ (equivFin a * equivFin b : ℤ)), by
     rw [mem_unitaryGroup_iff, star_smul, star_trivial, smul_mul_smul]
     ext i j
     have (x : Fin (2^n)) (y) := mul_comm ((starRingEnd ℂ) (ζ (2^n) ^ (equivFin i * x : ℤ))) y
-    simp_all [← mul_inv, mul_apply, one_apply, sum_register_univ_eq]
-    grind
+    simp_all [← mul_inv, mul_apply, one_apply, sum_register_univ_eq, show i = j ↔ j = i from Eq.comm]
   ⟩
