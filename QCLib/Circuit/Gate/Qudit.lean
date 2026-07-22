265a40a78bf5ddd3e8d9172368c1189c60d69985
DHTT <prparadoxies@gmail.com>
2026-07-22 14:01:26 +0200

    .

--- QCLib/Circuit/Gate/Qudit.lean
+++ QCLib/Circuit/Gate/Qudit.lean
@@ -2,10 +2,7 @@ module
 
 public import QCLib.LinearAlgebra.StdBasis
 public import QCLib.LinearAlgebra.Unitary
-
-public import Mathlib.Analysis.CStarAlgebra.Matrix
 public import QCLib.LinearAlgebra.UnitaryGroup.RootsOfUnity
-public import Mathlib.Logic.Equiv.Fin.Rotate
 
 @[expose] public noncomputable section
 
@@ -17,12 +14,23 @@ attribute [simp ←] map_pow
 
 notation "𝐔ᶠ["n"]" => unitary (EuclideanSpace ℂ n →L[ℂ] EuclideanSpace ℂ n)
 
+theorem orderOf_finRotate [hd : d.AtLeastTwo] :
+    orderOf (finRotate d) = d := by
+  simp [(isCycle_finRotate_of_le hd.prop).orderOf, support_finRotate_of_le hd.prop]
+
+
 def Z : 𝐔ᶠ[Fin d] := diagonalMonoidHom (fun k => (uζ d) ^ (k : ℕ))
+
+def X : 𝐔ᶠ[Fin d] := permHom (finRotate d)
 
 @[simp]
 theorem Z_pow [NeZero d] : (Z d) ^ d = 1 := by
   ext
   simp [Z, pow_right_comm]
+
+@[simp]
+theorem X_pow [hd : d.AtLeastTwo] : (X d) ^ d = 1 := by
+  simp [X, -permHom_apply, ((orderOf_eq_iff hd.toNeZero.pos).mp (orderOf_finRotate d)).left]
 
 theorem orderOf_Z [hd : d.AtLeastTwo] : orderOf (Z d) = d :=
   (orderOf_eq_iff hd.toNeZero.pos).mpr (by
@@ -34,4 +42,10 @@ theorem orderOf_Z [hd : d.AtLeastTwo] : orderOf (Z d) = d :=
     simpa [Nat.mod_eq_of_lt hd.one_lt] using congrFun hz 1
   )
 
-def X : 𝐔ᶠ[Fin d] := permHom (finRotate d)
+theorem orderOf_X [hd : d.AtLeastTwo] : orderOf (X d) = d :=
+  (orderOf_eq_iff hd.toNeZero.pos).mpr (by
+    simp only [X_pow, ne_eq, true_and]
+    intro m hmd hm hx
+    simp [X] at hx
+    sorry
+  )
